43fb42c6aa5af48f9829380c3c3e9a970c980d19
SoleMina <karina.pradogutierrez@gmail.com>
2016-05-11 06:19:00 -0500

    terminado

---
+++ main.js
@@ -0,0 +1,53 @@
+function getInputValue() {
+	var inputBox = document.getElementById("inputBox");
+	return inputBox.value;
+}
+function task() {
+	var inputBoxValue = getInputValue();
+	if (inputBoxValue !== "") {
+		doTask(inputBoxValue);
+		clean();
+	}
+}
+function doTask(taskText) {
+	var task = document.getElementById("tasks");
+	//Crear el elemento
+	var elemento = document.createElement("div");
+
+	elemento.className = "task";
+	elemento.innerHTML = '<input id="tachado" class="check" type="checkbox" value="checkbox"  onclick="tachar()">'+'</input><i id="cero" class="glyphicon glyphicon-trash"></i>'+taskText;
+	//Agregar elementos a tasks
+
+	tasks.appendChild(elemento);
+	var ex = tasks.lastChild;
+	alert(ex.innerHTML);
+
+	cero.onclick = function(){
+		task.removeChild(elemento);
+	}
+}
+function clean() {
+	var inputBox = document.getElementById("inputBox");
+	inputBox.value = "";
+	inputBox.focus();
+}
+function validate(){
+var input = document.getElementById("inputBox");
+if(input.value.length < 5) {
+alert('Escribe más de 5 carácteres.');
+}else{
+   document.getElementById(task());
+}
+}
+/*
+function tachar(){
+	var tachado=document.getElementById("tachado").checked; //Checked: para que funcione la casilla de verificación como true o false.
+	if(tachado==true){
+		document.getElementById("tachado").strike();
+	}
+}
+function tachar(){
+    var element= document.getElementById("tachado").strike();
+    return element.value;
+}
+*/
